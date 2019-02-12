ef46167e967deac118ac820fad4fe8888e42bfbf
Anay Wadhera <anay1018@gmail.com>
2019-02-11 18:44:32 -0800

    CustomDoze: refactor for statix classpaths

--- src/com/custom/ambient/display/DozeBrightness.java
+++ src/com/custom/ambient/display/DozeBrightness.java
@@ -31,7 +31,7 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
-import com.dirtyunicorns.support.preferences.CustomSeekBarPreference;
+import com.statix.support.preferences.CustomSeekBarPreference;
 
 public class DozeBrightness extends PreferenceActivity implements PreferenceFragment.OnPreferenceStartFragmentCallback {
 
